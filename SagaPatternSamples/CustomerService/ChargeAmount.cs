44a6da3ee5f737d35ab440a12f3e5975869de595
vishwanathsrikanth <vism@microsoft.com>
2019-06-09 22:37:41 +0530

    added samples for saga

---
+++ SagaPatternSamples/CustomerService/ChargeAmount.cs
@@ -0,0 +1,45 @@
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Models;
+using Newtonsoft.Json;
+using System.Text;
+using System.Linq;
+
+namespace PaymentService
+{
+    public static class ChargeAmount
+    {
+        const string serviceBusConnectionString = "Endpoint=sb://srikssb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=bVQQW4ryKmFti0GTbX5YLPEbcsuGZCREOiN9ocGrzR8=";
+        const string cancelOrderTopicName = "CancelOrder";
+        static ITopicClient cancelOrderTopicClient;
+
+        static ChargeAmount()
+        {
+            cancelOrderTopicClient = new TopicClient(serviceBusConnectionString, cancelOrderTopicName);
+        }
+
+        [FunctionName("ChargeAmount")]
+        public static void Run([ServiceBusTrigger("ChargeAmount", "chargeamountsubscription", 
+            Connection = "ServiceBusConnection")]Message message, ILogger log)
+        {
+            var order = JsonConvert.DeserializeObject<OrderModel>(Encoding.UTF8.GetString(message.Body));
+            var customerAccount = PaymentRepository.customerAccountModels.FirstOrDefault(c => c.CustomerId == order.CustomerId);
+            var orderValue = order.Price * order.Quantity;
+            if(orderValue <= customerAccount.MaximumOrderLimit)
+            {
+                log.LogInformation("Payment successful");
+            }
+            else
+            {
+                var messageBody = new CancelOrderModel();
+                messageBody.TrackingId = order.TrackingId;
+                messageBody.Message = "Order cannot be process as it exceeded the maximum order set for customer";
+                var cancelOrderMessage = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageBody)));
+                // Send the message to the topic and assume it will be successfull - eventual consistency
+                cancelOrderTopicClient.SendAsync(cancelOrderMessage).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
---
+++ SagaPatternSamples/CustomerService/PaymentRepository.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentService
+{
+    public static class PaymentRepository
+    {
+        public static IList<CustomerAccountModel> customerAccountModels = 
+            new List<CustomerAccountModel>();
+
+        static PaymentRepository()
+        {
+            var customerAccountModel = new CustomerAccountModel { CustomerId = 1, MaximumOrderLimit = 1000 };
+            customerAccountModels.Add(customerAccountModel);
+        }
+    }
+}
